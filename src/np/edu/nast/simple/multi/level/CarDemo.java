b9ace4c9d321f4032959dd5845c798a63aafad85
nastcollege <nastcollegegit@gmail.com>
2024-11-28 09:47:09 +0545

    Simple inheritance with constructor method and property as member

---
+++ src/np/edu/nast/simple/multi/level/CarDemo.java
@@ -0,0 +1,11 @@
+package np.edu.nast.simple.multi.level;
+
+public class CarDemo {
+    public static void main(String[] args) {
+        RacingCar rc = new RacingCar();
+        rc.on();
+        rc.run();
+        rc.race();
+        rc.off();
+    }
+}
---
+++ src/np/edu/nast/simple/multi/level/Engine.java
@@ -0,0 +1,11 @@
+package np.edu.nast.simple.multi.level;
+
+public class Engine {
+
+    void on(){
+        System.out.println("On");
+    }
+    void off(){
+        System.out.println("Off");
+    }
+}
