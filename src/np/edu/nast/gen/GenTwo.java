42afac58044307d2ea7238dda93a1e2d7934aefc
nastcollege <nastcollegegit@gmail.com>
2025-02-01 12:43:35 +0545

    Generics

---
+++ src/np/edu/nast/gen/GenTwo.java
@@ -0,0 +1,15 @@
+package np.edu.nast.gen;
+
+import java.lang.reflect.GenericArrayType;
+
+public class GenTwo<K,V>{
+    private K k;
+    private V v;
+
+    GenTwo(K k, V v){
+        this.k=k;
+        this.v=v;
+    }
+
+
+}
